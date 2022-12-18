c39483c16396b4cb8722402760fd90c342f33010
MD. MONIRUZZAMAN <53488876+zamanmonirbu@users.noreply.github.com>
2022-12-18 15:20:23 +0600

    making function in differnet process

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function add(  n1, n2=20)
-{
+//process one
+function function1( n1,n2){
     return n1+n2;
 }
-console.log(add(2));
+f1=function1(1,2);
+console.log(f1);
+
+//process two
+let function2= function ( n1,n2){
+    return n1+n2;
+}
+f2=function2(1,2);
+console.log(f2);
+
+//process three
+let function3=(n1,n2)=> n1+n2;
+console.log(function3(1,2));
