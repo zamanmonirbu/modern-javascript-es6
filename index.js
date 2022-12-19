1a2d569eb993ffbbacdf331efadb667c6b5a1fa9
MD. MONIRUZZAMAN <53488876+zamanmonirbu@users.noreply.github.com>
2022-12-19 13:07:12 +0600

    multiple arry

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-//Class object
- class Student{
-        constructor(name,roll){
-            this.sName=name;
-            this.sRoll=roll;
-            this.school="Mominpur";
-        }
- }
-let Student1=new Student("MONIR",19);
-let Student2=new Student("GONI",19);
-console.log(Student1,Student2);
+//concate multiple arry
+
+let arry1=[1,5,3,6,7];
+let arry2=[13,556,356,656,76];
+let arry3=[543,546,356,656,76];
+
+let first_cocate=arry1.concat(arry2).concat(arry3);
+console.log(first_cocate);
+
+let second_arry=[...arry1,...arry2,...arry3];
+console.log(second_arry);
