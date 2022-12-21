583b0c8c81ede5210edfab02b556ce2de6217c02
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2022-12-22 00:46:13 +0600

    polymarphism

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-//concate multiple arry
+//inheritance;
+class parent{
+    constructor(){
+        this.fatherName="Mintu";
+    }
+}
+class child extends parent{
+    constructor(){
+        super();
+        this.sonName="monir";
+        this.doughterName="Monira";
+    }
+}
 
-let arry1=[1,5,3,6,7];
-let arry2=[13,556,356,656,76];
-let arry3=[543,546,356,656,76];
-
-let first_cocate=arry1.concat(arry2).concat(arry3);
-console.log(first_cocate);
-
-let second_arry=[...arry1,...arry2,...arry3];
-console.log(second_arry);
-console.log(second_arry);
+let information=new child();
+console.log(information);
