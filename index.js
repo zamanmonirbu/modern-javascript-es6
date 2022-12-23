a92e9ab231f8a7287d9625b9c157309de8ca76a1
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2022-12-23 08:33:33 +0600

    True or flase

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-//Easy object access
-const information={name:"monir",Gf:"NO",phone:"01400650261",address:{vill:'kaute',upzela:'ghatail'}};
+//Null value
+const v1="";
+const v2=0;
+const v3=null;
+const v4=false;
+const v5=undefined;
+const v6=NaN;
 
+if(v1||v2||v3||v4||v5||v6){
+    console.log("True value");
+}
+else{
+    console.log("False value");
+}
 
-const {vill}=information.address;
-const {name}=information;
-
-
-console.log(vill);
+// console.log(v1,v2,v3,v4,v5);
