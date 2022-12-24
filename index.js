65c72ee8fe55e30b1b048ef9bbc862a7a41baf3c
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2022-12-24 21:53:50 +0600

    JSON

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-//Array slice, splice, array join 
-let arr=[1,4,4,5,5,3,6,7,9,9,53,5,3,45,6,3];
-let slice1=arr.slice(2,5);
-let splice2=arr.splice(1,2);
-let Join1=arr.join(" ",2);
+//24-4 JSON, JSON Structure, parse, stringify, JSON properties
+let information={
+    name:"Monir",vill:"Kaute Nogor",Pn:0172727727
+};
 
-console.log(slice1);
-console.log(splice2);
-console.log(Join1);
+let AtJ=JSON.stringify(information);
+let JtA=JSON.parse(AtJ);
+
+console.log(AtJ);
+console.log(JtA);
