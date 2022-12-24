77c8f280db73e5d9e7aa64067b49805e69128080
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2022-12-24 09:32:06 +0600

    variable scope

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-//  Apply map, filter, find on an array of objects
-const student=[
-    { roll:21,name:"monir"},
-    { roll:31,name:"Jahir"},
-    { roll:41,name:"Halim"},
-    { roll:51,name:"Galim"}
-
-],another=[];
-
-
-// for(let i=0;i<student.length;i++){
-//     let content=student[i].name;
-//     another[i]=content;
-
-// }
-
-const name=student.map( s => s.name);
-
-console.log(name);
+//variuable Scope
+function addition(n1,n2){
+    let resultf=n1+n2;
+    return resultf;
+}
+let result=addition(5,5);
+console.log(result);
+// console.log(resultf); 
+//can't use because var not define this scope
