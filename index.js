5eebdf88e1b25f0aa7946576be8cca4f96af4efe
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2022-12-24 21:48:43 +0600

    slice

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-//variuable Scope
-function addition(n1,n2){
-    let resultf=n1+n2;
-    return resultf;
-}
-let result=addition(5,5);
-console.log(result);
-// console.log(resultf); 
-//can't use because var not define this scope
+//Array slice, splice, array join 
+let arr=[1,4,4,5,5,3,6,7,9,9,53,5,3,45,6,3];
+let slice1=arr.slice(2,5);
+let splice2=arr.splice(1,2);
+let Join1=arr.join(" ",2);
+
+console.log(slice1);
+console.log(splice2);
+console.log(Join1);
